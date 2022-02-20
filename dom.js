d7ff7b4353cdd4694a075b8885a09ad2760176df
majharul224 <majharulislam2225@gmail.com>
2022-02-20 19:16:30 +0600

    first commit

--- dom.js
+++ dom.js
@@ -4,7 +4,7 @@ document.getElementById('add-border').addEventListener('click',function(){
    backgroundColor.style.border='5px solid red';
 })
 
-// onClick border
+// onClick background
 function addBackgroundColor(){
     const friends = document.getElementsByClassName('friend');
     for(const friend of friends){
@@ -12,11 +12,21 @@ function addBackgroundColor(){
     }
 }
 
-// onClick border id
-document.getElementById('add-broder').addEventListener('click',function(){
+// onClick background id
+document.getElementById('add-backgr').addEventListener('click',function(){
     // console.log('add-broder');
     const friends = document.getElementsByClassName('friend');
     for(const friend of friends){
         friend.style.backgroundColor='blue';
     }
+})
+// add friend
+document.getElementById("add-friend").addEventListener('click',function(){
+    const container = document.getElementById('containar');
+    const friendDiv = document.createElement('p');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML= `<h2>Lorem, ipsum.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dolore.</p>
+    `;
+    container.appendChild(friendDiv);
 })
