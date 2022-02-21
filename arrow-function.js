2318b8f1f5f0ebb4e62f025977fb24bfd182ef51
majharul224 <majharulislam2225@gmail.com>
2022-02-21 15:51:39 +0600

    first commit

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+// function declaration
+/* function add(num1,num2){
+    return num1 + num2;
+}
+const sum1 =add(15,17);
+console.log(sum1); */
+
+// function expression
+/* const add2 = function add(num1, num2){
+    return num1 + num2;
+}
+const sum2 = add2(15,17);
+console.log(sum2); */
+
+
+
+// function expression (anoymous)
+/* const add3 = function (num1, num2){
+    return num1 + num2;
+}
+const sum3 = add3(15,17);
+console.log(sum3); 
+ */
+
+//arrow function
+const add4 = (num1,num2) =>num1 + num2;
+const sum4 = add4(15,17);
+console.log(sum4);
+
